0aa6aaf7fadc73660e002785223bb287d12f4e19
AnjaliMurugan22 <anjali.ananthi22@gmail.com>
2019-09-27 22:55:22 +0530

    first commit

---
+++ js/jquery.js
@@ -0,0 +1,141 @@
+
+// Step1:Menu script
+
+//menu toggle button
+$(document).ready(function(){
+    $(".menu-icon").on("click",function(){
+        $("nav ul").toggleClass("showing");
+    })
+});
+//scrolling effect
+$(window).on("scroll", function(){
+    if($(window).scrollTop()){
+        $('nav').addClass('black');
+    }
+    else{
+        $('nav').removeClass('black');
+    }
+});
+
+
+
+// Step2:owl carosel
+//::  autoplay for slide second
+//::  nav:false for <>(prev and next) disappear
+//::  dots:false for . disappear
+
+// 1st owlCarousel
+$(document).ready(function(){
+    $('.owl1').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots:true,
+        autoplay:1000,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+    // prev and next button
+    var owl = $('.owl1');
+    owl.owlCarousel();
+    $('.customNextBtn').click(function() {
+        owl.trigger('next.owl.carousel');
+    })
+    $('.customPrevBtn').click(function() {
+        owl.trigger('prev.owl.carousel', [300]);
+    })
+})
+
+
+// 2nd owlCarousel
+$(document).ready(function(){
+    $('.owl2').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:5
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+    
+    var owl = $('.owl2');
+    owl.owlCarousel();
+    $('.customNextBtn1').click(function() {
+        owl.trigger('next.owl.carousel');
+    })
+    $('.customPrevBtn1').click(function() {
+        owl.trigger('prev.owl.carousel', [300]);
+    })
+    
+})
+
+
+// Product Gallery with Image zoom
+$('.show').zoomImage();
+$('.show-small-img:first-of-type').css({'border': 'solid 1px #951b25', 'padding': '2px'})
+$('.show-small-img:first-of-type').attr('alt', 'now').siblings().removeAttr('alt')
+$('.show-small-img').click(function () {
+  $('#show-img').attr('src', $(this).attr('src'))
+  $('#big-img').attr('src', $(this).attr('src'))
+  $(this).attr('alt', 'now').siblings().removeAttr('alt')
+  $(this).css({'border': 'solid 1px #951b25', 'padding': '2px'}).siblings().css({'border': 'none', 'padding': '0'})
+  if ($('#small-img-roll').children().length > 4) {
+    if ($(this).index() >= 3 && $(this).index() < $('#small-img-roll').children().length - 1){
+      $('#small-img-roll').css('left', -($(this).index() - 2) * 76 + 'px')
+    } else if ($(this).index() == $('#small-img-roll').children().length - 1) {
+      $('#small-img-roll').css('left', -($('#small-img-roll').children().length - 4) * 76 + 'px')
+    } else {
+      $('#small-img-roll').css('left', '0')
+    }
+  }
+})
+// b
+$('#next-img').click(function (){
+  $('#show-img').attr('src', $(".show-small-img[alt='now']").next().attr('src'))
+  $('#big-img').attr('src', $(".show-small-img[alt='now']").next().attr('src'))
+  $(".show-small-img[alt='now']").next().css({'border': 'solid 1px #951b25', 'padding': '2px'}).siblings().css({'border': 'none', 'padding': '0'})
+  $(".show-small-img[alt='now']").next().attr('alt', 'now').siblings().removeAttr('alt')
+  if ($('#small-img-roll').children().length > 4) {
+    if ($(".show-small-img[alt='now']").index() >= 3 && $(".show-small-img[alt='now']").index() < $('#small-img-roll').children().length - 1){
+      $('#small-img-roll').css('left', -($(".show-small-img[alt='now']").index() - 2) * 76 + 'px')
+    } else if ($(".show-small-img[alt='now']").index() == $('#small-img-roll').children().length - 1) {
+      $('#small-img-roll').css('left', -($('#small-img-roll').children().length - 4) * 76 + 'px')
+    } else {
+      $('#small-img-roll').css('left', '0')
+    }
+  }
+})
+// c
+$('#prev-img').click(function (){
+  $('#show-img').attr('src', $(".show-small-img[alt='now']").prev().attr('src'))
+  $('#big-img').attr('src', $(".show-small-img[alt='now']").prev().attr('src'))
+  $(".show-small-img[alt='now']").prev().css({'border': 'solid 1px #951b25', 'padding': '2px'}).siblings().css({'border': 'none', 'padding': '0'})
+  $(".show-small-img[alt='now']").prev().attr('alt', 'now').siblings().removeAttr('alt')
+  if ($('#small-img-roll').children().length > 4) {
+    if ($(".show-small-img[alt='now']").index() >= 3 && $(".show-small-img[alt='now']").index() < $('#small-img-roll').children().length - 1){
+      $('#small-img-roll').css('left', -($(".show-small-img[alt='now']").index() - 2) * 76 + 'px')
+    } else if ($(".show-small-img[alt='now']").index() == $('#small-img-roll').children().length - 1) {
+      $('#small-img-roll').css('left', -($('#small-img-roll').children().length - 4) * 76 + 'px')
+    } else {
+      $('#small-img-roll').css('left', '0')
+    }
+  }
+})
